6d8c7c4df88b0d4d5523459cc6e765f36ca55f1b
RainHard23 <moiredmi7@gmail.com>
2023-09-01 07:00:32 +0300

    CheckBox/TextField

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,23 @@
 import { useLayoutEffect } from 'react'
 
+import { CheckboxPrimitive } from '@/components/ui/checkbox/checkbox'
+import { TextField } from '@/components/ui/textfield/textfield'
+
 export const App = () => {
   useLayoutEffect(() => {
     document.body.setAttribute('data-theme', 'dark')
   })
 
-  return <></>
+  return (
+    <>
+      <CheckboxPrimitive checked={false} label={'kekek'} />
+      <TextField
+        label={'input'}
+        type={'password'}
+        errorMessage={'lol'}
+        placeholder={'Введите текст'}
+        className={'s.search'}
+      />
+    </>
+  )
 }
